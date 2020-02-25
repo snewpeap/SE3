47a6136d8f18f460a9ac89f6bac8e67fa56ee7d6
171250524 <171250524@smail.nju.edu.cn>
2020-02-25 14:05:37 +0800

    定义了业务逻辑层的相关接口，相关的VO对象暂用Object代替

--- src/main/java/edu/nju/se/teamnamecannotbeempty/backend/service/paper/PaperService.java
+++ src/main/java/edu/nju/se/teamnamecannotbeempty/backend/service/paper/PaperService.java
@@ -1,4 +1,32 @@
 package edu.nju.se.teamnamecannotbeempty.backend.service.paper;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ *定义论文检索相关的业务逻辑接口
+ * @author 赖宝光
+ * @date 2020.2.25
+ */
+@Service
 public interface PaperService {
+
+    /**
+     *根据用户的检索条件返回所需的结果列表
+     * @param text 用户输入的检索字段类型
+     * @param mode 用户选择的搜索字段
+     * @param pageNumber 发出请求的页号
+     * @param sortMode 用户需要的结果列表的排序方式
+     * @param perPage 每页的显示数量
+     * @return List<SimplePaperVO> 一个论文简略信息的VO对象的List
+     */
+    List<Object> search(String text, String mode, int pageNumber, String sortMode, int perPage);
+
+    /**
+     * 根据id返回用户所要查看的论文的详细信息
+     * @param id 论文id
+     * @return PaperVO 论文详细信息的VO对象
+     */
+    Object getPaper(int id);
 }
