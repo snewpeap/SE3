3103ad4f7f8ab0704adc42477cf985d78a8866a2
171250524 <171250524@smail.nju.edu.cn>
2020-03-30 16:23:31 +0800

    补充了service层的可视化部分接口，admincontroller有小部分修改

--- backend-main/src/main/java/edu/nju/se/teamnamecannotbeempty/backend/service/visualization/EntityService.java
+++ backend-main/src/main/java/edu/nju/se/teamnamecannotbeempty/backend/service/visualization/EntityService.java
@@ -1,4 +1,31 @@
 package edu.nju.se.teamnamecannotbeempty.backend.service.visualization;
 
+import edu.nju.se.teamnamecannotbeempty.backend.vo.AcademicEntityVO;
+import edu.nju.se.teamnamecannotbeempty.backend.vo.GraphVO;
+
 public interface EntityService {
+
+    /**
+     * 获取学术实体
+     * @param id 学术实体ID
+     * @param type 学术实体种类
+     * @return AcademicEntityVO 学术实体的VO
+     */
+    AcademicEntityVO getAcedemicEntity(long id, int type);
+
+    /**
+     * 获取学术实体的基础关系图（限定了连接距离，好像是2）
+     * @param id 学术实体ID
+     * @param type 学术实体种类
+     * @return GraphVO 关系图的VO
+     */
+    GraphVO getBasicGraph(long id, int type);
+
+    /**
+     * 获取学术实体的完整的关系图
+     * @param id 学术实体ID
+     * @param type 学术实体种类
+     * @return GraphVO 关系图的VO
+     */
+    GraphVO getCompleteGraph(long id, int type);
 }
