5d208a03e438dfab5e525b4d8324f28f348ef0ea
YRD <1127199932@qq.com>
2020-07-05 18:19:14 +0800

    更准确识别机构名；新的dump script

--- batch/src/main/java/edu/nju/se/teamnamecannotbeempty/batch/parser/csv/converters/ToAffiliation.java
+++ batch/src/main/java/edu/nju/se/teamnamecannotbeempty/batch/parser/csv/converters/ToAffiliation.java
@@ -2,6 +2,7 @@ package edu.nju.se.teamnamecannotbeempty.batch.parser.csv.converters;
 
 import com.opencsv.bean.AbstractCsvConverter;
 import edu.nju.se.teamnamecannotbeempty.data.domain.Affiliation;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,12 +15,20 @@ import java.util.Map;
 
 public class ToAffiliation extends AbstractCsvConverter {
     private static final HashMap<String, Affiliation> saveMap = new HashMap<>();
+    private static final Affiliation na;
     private static Map<String, String> synonyms = new HashMap<>();
+
+    static {
+        na = new Affiliation();
+        na.setName("NA");
+        na.setFormattedName("NA");
+        saveMap.put("NA", na);
+    }
 
     @Override
     public Object convertToRead(String value) {
-        value = value.trim();
-        if (value.isEmpty()) return null;
+        value = StringUtils.abbreviateMiddle(value.trim(), "...", 254);
+        if (StringUtils.isBlank(value) || value.equals(",")) return na;
         String formatted = value;
         for (Map.Entry<String, String> entry : synonyms.entrySet()) {
             formatted = formatted.replaceAll(entry.getKey(), entry.getValue());
